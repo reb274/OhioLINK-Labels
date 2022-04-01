08d182ae188c399e4506ae5bd082f98e0c06f932
dwgreen <david@davidwgreen.com>
2022-04-01 08:41:19 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,140 @@
+let source;
+let destination;
+let db;
+let data;
+let matches;
+
+function addCustomLabel() {
+
+    // Add new custom record to db
+    var id = 99999;
+    var libid = String(document.getElementById('custom-libid').value);
+    var name = document.getElementById('custom-name').value;
+    var address = document.getElementById('custom-address').value;
+    var city = document.getElementById('custom-city').value;
+    var state = document.getElementById('custom-state').value;
+    var zip = document.getElementById('custom-zip').value;
+
+    if (id > 0) {
+        db.insert({
+            "id": parseInt(id),
+            "libid": libid,
+            "name": name,
+            "address": address,
+            "city": city,
+            "state": state,
+            "zip": parseInt(zip),
+            "seo": null,
+            "hub": null,
+            "route": null
+        });
+
+        let x = document.createElement("option");
+        x.text = name + " (" + libid + ") ";
+        x.value = id;
+        document.getElementById("source").add(x);
+        document.getElementById("source").value = id;
+
+        getLabelData();
+    }
+
+    this_modal.hide();
+
+    return false;
+}
+
+function getLabelData() {
+    if (!matches) {
+        // Generate NodeList of all label data elements
+        matches = window.frames["output"].contentDocument.querySelectorAll('[data-label]');
+    }
+
+    source = db({id: parseInt(document.labels.source.value)}).first();
+    destination = db({id: parseInt(document.labels.destination.value)}).first();
+
+    for (const e of matches) {
+        switch (e.dataset.label) {
+            case "from-id":
+                e.textContent = ["FROM:", source.libid].join(" ");
+                break;
+            case "from-name":
+                e.textContent = source.name;
+                break;
+            case "from-address-1":
+                e.textContent = source.address;
+                break;
+            case "from-address-2":
+                e.textContent = [source.city, source.state, source.zip].join(" ");
+                break;
+            case "to-id":
+                e.textContent = ["TO:", destination.libid].join(" ");
+                break;
+            case "to-name":
+                e.textContent = destination.name;
+                break;
+            case "to-address-1":
+                e.textContent = destination.address;
+                break;
+            case "to-address-2":
+                e.textContent = [destination.city, destination.state, destination.zip].join(" ");
+                break;
+            case "hub":
+                e.textContent = destination.hub;
+                break;
+            case "route-id":
+                e.textContent = destination.route;
+                break;
+            case "seo":
+                // empty if no value
+                e.textContent = destination.seo ? ["SEO:", destination.seo].join(" ") : "";
+                break;
+            default:
+                e.textContent = 'ERROR'
+        }
+    }
+}
+
+function printLabels() {
+    window.frames["output"].focus();
+    window.frames["output"].contentWindow.print();
+}
+
+// Populate the form dropdowns
+function loadOptions() {
+    db().each(function (record) {
+        let x, y;
+        x = document.createElement("option");
+        y = document.createElement("option");
+        x.text = y.text = record["name"] + " (" + record["libid"] + ") ";
+        x.value = y.value = record["id"];
+        document.getElementById("source").add(x);
+        document.getElementById("destination").add(y);
+    });
+}
+
+// Initialise TaffyDB from JSON file
+function initDB() {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            data = JSON.parse(this.responseText);
+            db = TAFFY(data);
+            db.sort("name"); // Alphabetize
+            loadOptions();
+        }
+    };
+    xmlhttp.open("GET", "output.json", true);
+    xmlhttp.send();
+}
+
+// Small helper for modal form focus
+let myModal = document.getElementById('custom-label');
+let myInput = document.getElementById('custom-libid');
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus();
+});
+
+// Needed for toggling modal visibility
+let this_modal = new bootstrap.Modal(document.getElementById('custom-label'), {
+    keyboard: false
+})
